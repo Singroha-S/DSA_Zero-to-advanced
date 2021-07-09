4aa62689a6736e7367f0f51e9d9676e1f6703bd3
Sahil Singroha <59840531+Singroha-S@users.noreply.github.com>
2021-07-09 22:39:08 +0530

    Create primes till N.cpp

---
+++ primes till N.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int N;
+    cin>>N;
+    for(int i=2;i<N;i++){
+        int div=2;
+        if(i>div){
+            if(i%div==0){
+                i++;
+            }
+            else{
+                div=div+1;
+            }
+        }
+        cout<<i<<" ";
+    }
+    return 0;
+}
+
