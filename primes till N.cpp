6c65d58425df8513a59e399e4b18b5fbb7b2280a
Sahil Singroha <59840531+Singroha-S@users.noreply.github.com>
2021-07-10 22:13:43 +0530

    Update primes till N.cpp

--- primes till N.cpp
+++ primes till N.cpp
@@ -3,17 +3,20 @@ using namespace std;
 int main(){
     int N;
     cin>>N;
-    for(int i=2;i<N;i++){
+    for(int i=2;i<N;i++)
+    {
         int div=2;
-        if(i>div){
-            if(i%div==0){
+        if(i>div)
+        {
+            if(i%div==0)
+            {
                 i++;
             }
             else{
-                div=div+1;
-            }
-        }
-        cout<<i<<" ";
+                  div=div+1;
+                }
+         }
+          cout<<i<<" ";
     }
     return 0;
 }
