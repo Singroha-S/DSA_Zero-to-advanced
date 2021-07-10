a8307206570eae38bb4944235722b82866586c1d
Sahil Singroha <59840531+Singroha-S@users.noreply.github.com>
2021-07-10 10:18:50 +0530

    Create sum_positive.cpp

---
+++ sum_positive.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int sum=0,n;
+    cin>>n;
+    while(n>=0){
+        sum=sum+n;
+        cin>>n;
+    }
+    cout<<"sum is "<<sum;
+
+    return 0;
+
+}
