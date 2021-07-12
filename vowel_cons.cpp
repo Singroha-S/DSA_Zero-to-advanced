f88aef27e02487bb83fc35fd98ca27caf5057791
Sahil Singroha <59840531+Singroha-S@users.noreply.github.com>
2021-07-12 14:08:25 +0530

    Update vowel_cons.cpp

--- vowel_cons.cpp
+++ vowel_cons.cpp
@@ -14,3 +14,37 @@ int main(){
     }
     return 0;
 }
+
+/*
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char c;
+    cout<<"enter a caharacter: "<<endl;
+    cin>>c;
+    switch(c)
+    {
+     case 'a':
+         cout<<"vowel"<<endl;
+         break;
+     case 'e':
+        cout<<"vowel"<<endl;
+        break;
+     case 'i':
+        cout<<"vowel"<<endl;
+        break;
+     case 'o':
+        cout<<"vowel"<<endl;
+        break;
+    case 'u':
+        cout<<"vowel"<<endl;
+        break;
+    default:
+        cout<<"consonent";
+        break;
+
+    }
+    return 0;
+}
+*/
