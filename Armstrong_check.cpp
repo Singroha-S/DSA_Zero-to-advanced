3f1552cfbb026e10382a9f3b4514c463f09b2183
Sahil Singroha <59840531+Singroha-S@users.noreply.github.com>
2021-07-13 22:28:11 +0530

    Create Armstrong_check.cpp

---
+++ Armstrong_check.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int sum=0, orig=n;
+    while(n>0)
+    {
+        int a=n%10;
+        sum=sum + a*a*a;
+        n=n/10;
+    }
+    if(orig==sum)
+    {
+        cout<<"armstrong no.";
+    }
+    else
+    {
+        cout<<"not";
+    }
+
+    return 0;
+}
