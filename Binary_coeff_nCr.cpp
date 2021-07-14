e08f7835acaffba03c07314ded0223fb32797847
Sahil Singroha <59840531+Singroha-S@users.noreply.github.com>
2021-07-15 00:01:29 +0530

    Create Binary_coeff_nCr.cpp

---
+++ Binary_coeff_nCr.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int fact(int n)
+{
+    int a=1;
+    for(int i=2;i<=n;i++)
+    {
+        a*=i;
+    }
+    return a;
+}
+int main()
+{
+    int n,r;
+    cin>>n>>r;
+    int ans= fact(n)/(fact(n-r)*fact(r));
+    cout<<ans;
+
+    return 0;
+}
