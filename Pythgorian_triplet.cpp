ff191de9bede62511eccc6b025ca221c14f661d8
Sahil Singroha <59840531+Singroha-S@users.noreply.github.com>
2021-07-17 00:05:07 +0530

    Update Pythgorian_triplet.cpp

--- Pythgorian_triplet.cpp
+++ Pythgorian_triplet.cpp
@@ -1,39 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int pythagorian(int a,int b,int c)
+bool pythagorian(int a,int b,int c)
 {
     int x=max(a, max(b,c));
     int y,z;
 
     if(x==a)
     {
-        y==b;
-        z==c;
+        y=b;
+        z=c;
     }
     else if(x==b)
     {
-        y==a;
-        z==c;
+        y=a;
+        z=c;
     }
     else
     {
-        y==a;
-        z==b;
+        y=a;
+        z=b;
     }
     if(x*x == y*y + z*z)
     {
-        return 1;
+        return true;
     }
-        return 0;
+        return false;
 }
 
 int main()
 {
     int a,b,c;
     cin>>a>>b>>c;
-    cout<<pythagorian(a,b,c);
-
+    if(pythagorian(a,b,c))
+    {
+        cout<<"triplet hai";
+    }
+    else{
+        cout<<"nhi hai";
+    }
+    
     return 0;
 }
+
 
