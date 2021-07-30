9775e94f0a27ccdc9275930a3ffaadf0c0518d0b
Sahil Singroha <59840531+Singroha-S@users.noreply.github.com>
2021-07-30 11:50:05 +0530

    Create 2D_array_searching.cpp

---
+++ 2D_array_searching.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,m,b;
+    cin>>n>>m>>b;
+    int a[n][m];
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            cin>>a[i][j];
+        }
+    }
+     for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            if(a[i][j]==b)
+            {
+                cout<<i<<" "<<j<<endl;
+                cout<<"found";
+                return 0;
+            }
+        }
+    }
+    cout<<"not found";
+    return 0;
+}
