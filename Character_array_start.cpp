6551d7c5c7dc7e07f0f7424a0f17cfae52db8995
Sahil Singroha <59840531+Singroha-S@users.noreply.github.com>
2021-07-31 23:39:50 +0530

    Create Character_array_start.cpp

---
+++ Character_array_start.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    cin.ignore();
+
+    char c[n+1];
+    cin.getline(c,n);
+    cin.ignore();
+
+    cout<<c;
+
+    return 0;
+}
+
+/*
+{
+    int n;
+    cin>>n;
+    char c[n+1];
+    cin>>c;
+    cout<<c;
+
+    return 0;
+}
+*/
+/*
+{
+    char c[100]="Naveen";
+    cout<<c;
+
+    return 0;
+}
+*/
