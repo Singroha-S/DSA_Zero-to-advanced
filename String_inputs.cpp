b8f04bdf4562598de8d17c20adcf4ba827513ea6
Sahil Singroha <59840531+Singroha-S@users.noreply.github.com>
2021-08-03 13:25:08 +0530

    Create String_inputs.cpp

---
+++ String_inputs.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    getline(cin,s);
+    cout<<s<<endl;
+
+    string s1(2,'naveen');  //only print first char two timess
+    cout<<s1;
+    return 0;
+}
+/*{
+    //string in double quotes
+    string str="hello Naveen";
+    cout<<str;
+    return 0;
+}
+*/
+/*
+{
+    //string input before space
+    string str;
+    cin>>str;
+    cout<<str;
+    return 0;
+}
+*/
