2bc7e2c4db9a4cc6c59df19c3aa1c31bd1b59626
Sahil Singroha <59840531+Singroha-S@users.noreply.github.com>
2021-08-04 19:33:11 +0530

    Update Pointer_array.cpp

--- Pointer_array.cpp
+++ Pointer_array.cpp
@@ -3,12 +3,15 @@ using namespace std;
 int main()
 {
     int a[]={2,6,1,4,8};
+    cout<<a<<endl;
     cout<<*a<<endl;
     cout<<&a<<endl;
 
     int *p=a;
     for(int i=0;i<5;i++)
     {
+       //cout<<*p<<" ";
+       //p++;
         cout<<*(a+i)<<" ";    //a,*a,p,*p
     }
 
