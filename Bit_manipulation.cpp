45ddcbb6aef14c5aa4fa0b37060dd2c238148de3
Sahil Singroha <59840531+Singroha-S@users.noreply.github.com>
2021-08-06 23:20:34 +0530

    Create Bit_manipulation.cpp

---
+++ Bit_manipulation.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int getbit(int n, int pos)
+{
+    return((n & (1<<pos))!=0);
+}
+int setbit(int n, int pos)
+{
+    return (n | (1<<pos));
+}
+int clearbit(int n,int pos)
+{
+    int mask = ~(1<<pos);
+    return (n & mask);
+}
+int updatebit(int n, int pos, int val)
+{
+    int mask = ~(1<<pos);
+    n = n & mask;
+    return (n | (val<<pos));
+}
+int main()
+{
+    //cout<<getbit(5,2)<<endl;
+    //cout<<setbit(4,1);
+    //cout<<clearbit(5,2);
+    cout<<updatebit(5,1,1);
+
+    return 0;
+}
