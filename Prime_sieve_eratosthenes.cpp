a80b77ef4068ee8faf671694dde0409affb01704
Sahil Singroha <59840531+Singroha-S@users.noreply.github.com>
2021-08-11 22:13:23 +0530

    Create Prime_sieve_eratosthenes.cpp

---
+++ Prime_sieve_eratosthenes.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+void prime_sieve(int n)
+{
+    int prime[1000]={0};
+
+    for(int i=2;i<=n;i++)
+    {
+        if(prime[i]==0)
+        for(int j=i*i;j<=n;j+=i)
+        {
+            prime[j]=1;
+        }
+    }
+
+    for(int i=2;i<=n;i++)
+    {
+        if(prime[i]==0)
+        {
+            cout<<i<<" ";
+        }
+    }
+    cout<<endl;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    prime_sieve(n);
+
+    return 0;
+}
