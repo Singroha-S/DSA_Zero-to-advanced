3e977f77222c0d93243972ad3de27712e211bc55
Sahil Singroha <59840531+Singroha-S@users.noreply.github.com>
2021-08-27 22:18:05 +0530

    Update Backtracking_N_Queen.cpp

--- Backtracking_N_Queen.cpp
+++ Backtracking_N_Queen.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 bool isSafe(int** a,int x,int y,int n){
-    for(int row=0;row<x;row++){
+    for(int row=0;row<x;row++){        //ek hi col k liye hrr row me check krre honge
         if(a[row][y]==1){
             return false;
         }
